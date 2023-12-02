c49383838946170cddc36acec19624eece0866c2
23P31A0555 <147792372+23P31A0555@users.noreply.github.com>
2023-12-02 18:57:20 +0530

    #include<stdio.h> int main() {     int i,n,r,fact=0;     scanf("%d",&n);     for(i=1;i<n;i++)     {         if(n%i==0)         {             fact=fact+i;         }     }     if(fact==n)     {         printf("PERFECT");     }     else if(fact<n)     {         printf("DEFICIENT");     }     else if(fact>n)     {         printf("ABUNDANT");     } }

---
+++ PDA.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,r,fact=0;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            fact=fact+i;
+        }
+    }
+    if(fact==n)
+    {
+        printf("PERFECT");
+    }
+    else if(fact<n)
+    {
+        printf("DEFICIENT");
+    }
+    else if(fact>n)
+    {
+        printf("ABUNDANT");
+    }
+}
