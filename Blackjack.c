e2b667213325bae4f598e8b237417e0811bd79f8
23P31A0555 <147792372+23P31A0555@users.noreply.github.com>
2024-01-03 20:18:38 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d",&a,&b);     c=a+b;     if(c>=11)     {         printf("%d",21-c);     }     else     {         printf("-1");     }
    }

---
+++ Blackjack.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    if(c>=11)
+    {
+        printf("%d",21-c);
+    }
+    else
+    {
+        printf("-1");
+    }
+    
+}
