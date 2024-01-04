4669e34d61f5c3a9d0811778bd66ed98f251284a
23P31A0555 <147792372+23P31A0555@users.noreply.github.com>
2024-01-04 18:04:00 +0530

    #include <stdio.h> #include <ctype.h>
    int main() {
        char str[100];
        scanf("%99[^
    ]", str);
    
        int sum = 0;
    
        for (int i = 0; str[i] != NULL; i++) {
            if (isdigit(str[i])) {
                sum += str[i] - '0';
            }
        }
        printf("%d
    ", sum);
    
        return 0;
    }

---
+++ Sum_of_digits_in_a_String.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    scanf("%99[^
+]", str);
+
+    int sum = 0;
+
+    for (int i = 0; str[i] != NULL; i++) {
+        if (isdigit(str[i])) {
+            sum += str[i] - '0';
+        }
+    }
+    printf("%d
+", sum);
+
+    return 0;
+}
+
+
